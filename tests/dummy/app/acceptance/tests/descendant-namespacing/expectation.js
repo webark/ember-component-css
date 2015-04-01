c11435666bbf156ea5906c6667de948590b5a595
Dan Freeman <dfreeman@salsify.com>
2015-03-31 22:31:41 -0400

    Basic acceptance testing

---
+++ tests/dummy/app/acceptance/tests/descendant-namespacing/expectation.js
@@ -0,0 +1,15 @@
+export default {
+  styles: {
+    '[data-test-green]': {
+      color: 'rgb(0, 255, 0)'
+    },
+
+    '[data-test-black]': {
+      color: 'rgb(0, 0, 0)'
+    },
+
+    '[data-test-orange]': {
+      color: 'rgb(255, 127, 0)'
+    }
+  }
+};
