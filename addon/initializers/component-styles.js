b9627598990ad4bdd9752acc9b276af7a4e2e35d
Mark David Avery <thewebark@gmail.com>
2016-03-08 18:05:35 -0800

    Updated to maintain roughly the same api, but with minimum useage of private apis.
    Also enables source mapping and hot realoding,
    and sets us up for supporting in addon usage.
    
    * Need to add back in the accptance test after removed
      it due to such severe ember upgrade differences.
    * Need to add support for plain css useage
    * Need to better differentiate between pod-styles.scss, and pod-styles.sass,
      rather then just not accepting sass files and just scss styles instead.
    * Need better testing around empty states and precompilers other then sass

---
+++ addon/initializers/component-styles.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+import podNames from 'ember-component-css/pod-names';
+
+const {
+  Component,
+  ComponentLookup,
+} = Ember;
+
+export function initialize() {
+
+  ComponentLookup.reopen({
+    componentFor(name, owner) {
+      if (podNames[name] && !owner.application.hasRegistration('component:' + name)) {
+        owner.application.register('component:' + name, Component);
+      }
+      return this._super(...arguments);
+    }
+  });
+
+  Component.reopen({
+    addPodStyleName: Ember.on('init', function() {
+      if (this.tagName !== '' && this._debugContainerKey) {
+        const name = this._debugContainerKey.replace('component:', '');
+        if (podNames[name]) {
+          this.classNames.push(podNames[name]);
+        }
+      }
+    }),
+  });
+}
+
+export default {
+  name: 'component-styles',
+  initialize
+};
