24002985dbf0e31240e7e25a12ebcc2fa4a5d743
Mark David Avery <mark@webark.cc>
2017-07-30 21:14:01 -0700

    chore(upgrade): upgraded ember cli and all dependecies to latest verson

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,11 +1,7 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const {
-  Router,
-} = Ember;
-
-export default Router.extend({
+export default Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
 }).map(function() {
