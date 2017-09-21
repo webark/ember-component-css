24ab9eacc7b9fab616e372fa1ae87d21bec4a114
Ramon Kleiss <ramonkleiss@gmail.com>
2017-09-21 15:21:58 +0200

    Create the route-styles initializer

---
+++ app/initializers/route-styles.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+import podNames from 'ember-component-css/pod-names';
+import StyleNamespacingExtras from '../mixins/style-namespacing-extras';
+
+const {
+  $,
+  Route,
+  computed
+} = Ember;
+
+Route.reopen(StyleNamespacingExtras, {
+  activate() {
+    this._super(...arguments);
+
+    $('body').addClass(podNames[this.get('_componentIdentifier').replace('route:', '')] || '');
+  },
+
+  deactivate() {
+    this._super(...arguments);
+
+    $('body').removeClass(podNames[this.get('_componentIdentifier').replace('route:', '')] || '');
+  }
+});
+
+export function initialize() {}
+
+export default {
+  name: 'route-styles',
+  initialize
+};
