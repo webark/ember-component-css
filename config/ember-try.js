5036578db27917aaf19ce6e75bc4032175407732
Mark David Avery <mark@webark.cc>
2017-11-01 13:48:56 -0700

    test(ember-try): now that it's confirmed that the node version was what was breaking it. adding back in the other ember try scenerios. (would be nice to have more vrsions on ember source)

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,27 +3,97 @@ module.exports = {
   useYarn: true,
   scenarios: [
     {
-      name: 'ember-lts',
+      name: 'ember-1.13',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release-1-13'
+        },
+        resolutions: {
+          'ember': 'release-1-13'
+        }
+      },
       npm: {
         devDependencies: {
-          'ember-source': 'lts'
+          'ember-source': null
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.8',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#lts-2-8'
+        },
+        resolutions: {
+          'ember': 'lts-2-8'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.12.0'
         }
       }
     },
     {
       name: 'ember-release',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release'
+        },
+        resolutions: {
+          'ember': 'release'
+        }
+      },
       npm: {
         devDependencies: {
-          'ember-source': 'latest'
+          'ember-source': null
         }
       }
     },
     {
       name: 'ember-beta',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
+      },
       npm: {
         devDependencies: {
-          'ember-source': 'beta'
+          'ember-source': null
         }
+      }
+    },
+    {
+      name: 'ember-canary',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
+    },
+    {
+      name: 'ember-default',
+      npm: {
+        devDependencies: {}
       }
     }
   ]
