907d059e4a970d908bae74e16913143c183af74d
Mark David Avery <mark@webark.cc>
2018-02-05 17:39:16 -0800

    fix(module imports): moved functionality that belonged to just the addon, over to the addon. fix #259

--- addon/mixins/style-namespacing-extras.js
+++ addon/mixins/style-namespacing-extras.js
@@ -1,16 +1,3 @@
 import Mixin from '@ember/object/mixin';
-import { computed } from '@ember/object';
 
-export default Mixin.create({
-  _componentIdentifier: computed({
-    get() {
-      return (this._debugContainerKey || '').replace('component:', '');
-    }
-  }),
-
-  _shouldAddNamespacedClassName: computed({
-    get() {
-      return this.get('tagName') !== '' && this.get('styleNamespace');
-    }
-  }),
-});
+export default Mixin.create();
