bd1c5907cae4d9b6d531557f04fba87612b4756a
Mark David Avery <mark@webark.cc>
2019-05-23 10:15:12 -0700

    refactor(route processing): the  utility should not need to hanlde the building of routes, it should just recive an array

--- addon/utils/add-route-style-namespace.js
+++ addon/utils/add-route-style-namespace.js
@@ -1,23 +1,17 @@
 const ROUTE_CLASS_SET_PROPERTY_NAME = 'routeStyleNamespaceClassSet';
 const ROUTE_CLASS_SINGLE_PROPERTY_NAME = 'styleNamespace';
 
-export default function addRouteStyleNamespace(owner, routeNames) {
+export default function addRouteStyleNamespace(owner, routes) {
   const classes = [];
 
-  while (routeNames.length) {
-    const name = routeNames.join('.');
+  for (const name of routes) {
     const { styleNamespace } = owner.lookup(`styles:${name}`) || {};
+    const controller = owner.lookup(`controller:${name}`);
 
-    if (styleNamespace) {
-      const controller = owner.lookup(`controller:${name}`);
-
-      if (controller) {
-        controller.set(ROUTE_CLASS_SINGLE_PROPERTY_NAME, styleNamespace);
-        classes.push(styleNamespace);
-      }
+    if (styleNamespace && controller) {
+      controller.set(ROUTE_CLASS_SINGLE_PROPERTY_NAME, styleNamespace);
+      classes.push(styleNamespace);
     }
-
-    routeNames.pop();
   }
 
   owner.lookup('controller:application').set(ROUTE_CLASS_SET_PROPERTY_NAME, classes.join(' '));
