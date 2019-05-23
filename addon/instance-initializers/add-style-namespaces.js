d6b7d1808ba0c16d0d76771afbfb2dcd4e6a5f1e
Mark David Avery <mark@webark.cc>
2019-05-23 10:15:12 -0700

    refactor(route processing): the  utility should not need to hanlde the building of routes, it should just recive an array

--- addon/instance-initializers/add-style-namespaces.js
+++ addon/instance-initializers/add-style-namespaces.js
@@ -4,16 +4,22 @@ import addComponentStyleNamespace from 'ember-component-css/utils/add-component-
 export function initialize(appInstance) {
   const router = appInstance.lookup('service:router');
   router.on('routeDidChange', function(transition) {
-    addRouteStyleNamespace(appInstance, transition.to.name.split('.'));
+    addRouteStyleNamespace(appInstance, nestedRoutes(transition.to.name));
   });
 
   router.on('routeWillChange', function(transition) {
     if (/loading$/.test(transition.to.name) && transition.isActive) {
-      addRouteStyleNamespace(appInstance, transition.to.name.split('.'));
+      addRouteStyleNamespace(appInstance, nestedRoutes(transition.to.name));
     }
   });
 
   addComponentStyleNamespace(appInstance);
+}
+
+function nestedRoutes(routeName) {
+  return routeName.split('.').reduce(function(allRoutes, routePart) {
+    return allRoutes.concat(allRoutes.slice(-1).concat(routePart).join('.'));
+  }, []);
 }
 
 export default {
