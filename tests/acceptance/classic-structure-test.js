9ee4a9a7caa54c129297b5993d52497bb0c550e3
Mark David Avery <mark@webark.cc>
2019-06-24 11:15:30 -0700

    test: skipping tests for now to be picked up later

--- tests/acceptance/classic-structure-test.js
+++ tests/acceptance/classic-structure-test.js
@@ -1,21 +1,21 @@
 import { visit } from '@ember/test-helpers';
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 
 import styleForSetup from 'dummy/tests/setup/style-for';
 
-module('Acceptance | classic structure', function(hooks) {
+module.skip('Acceptance | classic structure', function(hooks) {
   setupApplicationTest(hooks);
   styleForSetup(hooks);
 
-  skip('should be able to use classic structure style', async function(assert) {
+  test('should be able to use classic structure style', async function(assert) {
     await visit('/classic-structure');
 
     const color = this.styleFor('.classic-structure').color;
     assert.equal(color, 'rgb(0, 0, 1)');
   });
 
-  skip('should be able to use classic structure style nested', async function(assert) {
+  test('should be able to use classic structure style nested', async function(assert) {
     await visit('/classic-structure-nested');
 
     const color = this.styleFor('.classic-structure-nested').color;
